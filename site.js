0bdb66bde2b58ddc36f3801915f73496f5313287
SuchABadCoder <gayday57@gmail.com>
2022-04-07 16:56:20 +0300

    Basketball countdown

--- site.js
+++ site.js
@@ -1,13 +1,34 @@
 var curDate = new Date();
-var d = [1,7,6,5,4,3,2];
-var endDate = new Date(today.getFullYear(),today.getMonth(),today.getDate()+ d[today.getDay()], 12, 0, 0);
-var clock = $(".clock").FlipClock(Math.abs(endDate - curDate) / 1000, {
-    clockFace: "DailyCounter",
-    countdown: true,
-    callbacks: {
-        stop:function() {
-          $('#message1').html('Merry Christmas!');
-          $('#message2').html('Time to smoke sess!');
-        }
+var endDate = setEndDate(curDate);
+setEndDate();
+
+function setEndDate() {
+    let dayRemains;
+    let curDate = new Date();
+    let daysBeforeTU = [4, 5, 6, 0, 1];
+
+    if (daysBeforeTU.indexOf(curDate.getDay()) != -1
+        && curDate.getHours() > 12
+        || (curDate.getHours() == 12 && curDate.getMinutes() >= 0)) {
+        dayRemains = [2, 1, 7, 6, 5, 4, 3]
     }
-});
+    else {
+        dayRemains = [4, 3, 2, 1, 7, 6, 5]
+    }
+
+    let endDate = new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate() + dayRemains[curDate.getDay()], 12, 0, 0);
+    startTimer(curDate, endDate);
+}
+
+function startTimer(curDate, endDate) {
+    var clock = $(".clock").FlipClock(Math.abs(endDate - curDate) / 1000, {
+        clockFace: "DailyCounter",
+        countdown: true,
+        callbacks: {
+            stop: function () {
+                $('#message2').html('Time to play basketball!');
+                setEndDate();
+            }
+        }
+    });
+}
